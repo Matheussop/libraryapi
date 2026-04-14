3ee8bd49e5b0215bb045095ed256ad377efab642
Matheus Luiz Oliveira Spindula <matheussopluiz@outlook.com>
2026-04-14 16:13:53 -0300

    Add Hypersistence Utils dependency and enhance security configuration with password encoding

--- src/main/java/io/github/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/libraryapi/config/SecurityConfiguration.java
@@ -2,10 +2,13 @@ package io.github.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,12 +21,17 @@ public class SecurityConfiguration {
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(configurer -> configurer.loginPage("/login").permitAll())
                 .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(authorizeRequests ->
-                        authorizeRequests
-                                .requestMatchers("/api/books/**").permitAll()
-                                .requestMatchers("/api/authors/**").permitAll()
-                                .anyRequest().authenticated()
-                )
+                .authorizeHttpRequests(authorizeRequests ->{
+                    authorizeRequests.requestMatchers(HttpMethod.POST,"/api/users/**").permitAll();
+                    authorizeRequests.requestMatchers("/api/books/**").permitAll();
+                    authorizeRequests.requestMatchers("/api/authors/**").permitAll();
+                    authorizeRequests.anyRequest().authenticated();
+                })
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
     }
 }
